fb8b126e295f18fe76e0592827b104c66ca0977c
Vova <tarabanvv@mail.ru>
2022-07-14 23:12:28 +0800

    catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,32 @@
-// $(document).ready(function(){
-  //   $('.carousel__inner').slick({
-  //     speed: 1200,
-  //     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left.png"></button>',
-  //     nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right.png"></button>',
-  //     responsive: [
-  //       {
-  //         breakpoint: 768,
-  //         settings: {
-  //           dots: true,
-  //           arrows: false
-  //         }
-  //       }
-  //     ]
-  //   });
-  // });
-
-  const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+      speed: 1200,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right.png"></button>',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: false
+          }
+        }
+      ]
+    });
   });
 
-document.querySelector('.prev').addEventListener('click', function () {
-  slider.goTo('prev');
-});
+//   const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+//   });
 
-document.querySelector('.next').addEventListener('click', function () {
-  slider.goTo('next');
-});
+// document.querySelector('.prev').addEventListener('click', function () {
+//   slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//   slider.goTo('next');
+// });
